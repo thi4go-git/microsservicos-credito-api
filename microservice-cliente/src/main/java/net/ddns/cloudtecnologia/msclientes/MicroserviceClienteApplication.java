aec25e2fdcf61c85e9711cd43f5ee2eee1da4660
Thiago Junior Amorim De Melo (GOVBR GO - DRC Central) <thiago.melo@govbr.com.br>
2023-01-14 13:41:07 -0300

    Commit inicial

---
+++ microservice-cliente/src/main/java/net/ddns/cloudtecnologia/msclientes/MicroserviceClienteApplication.java
@@ -0,0 +1,13 @@
+package net.ddns.cloudtecnologia.msclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceClienteApplication.class, args);
+	}
+
+}
